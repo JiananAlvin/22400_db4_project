26e1fb2006563a42199e8fc635a2e405083aeae4
JiananAlvin <jiananwang990@gmail.com>
2022-06-13 09:39:13 +0200

    publish data to Adafruit IO

--- stepper_motor/stepper_motor.py
+++ stepper_motor/stepper_motor.py
@@ -1,11 +1,28 @@
-
 from machine import Pin, PWM
+import time
+import utime
 
-pin = PWM(Pin(33))  # Step
-pinDir = Pin(27, Pin.OUT)  # Direction
+pinStep1 = PWM(Pin(33))  # Step
+pinDir1 = Pin(27, Pin.OUT)  # Direction
+pinStep2 = Pin(12, Pin.OUT)
+pinDir2 = Pin(13, Pin.OUT)
 
-pinDir(0)  # 0: counterclockwise, 1: clockwise
+pinDir1(1)  # 0: counterclockwise, 1: clockwise
+pinDir2(0)
 
-pin.freq(400)  # Frequency is the number of times per second that we repeat the on and off cycle -> rotating speed
-pin.duty(100)  # Duty cycle refers the amount of time the pulse is ON -> voltage
+# Initialize two stepper motors
+pinStep1.freq(0)
+pinStep1.duty(0)
+pinStep2.value(0)
+time.sleep(5)
 
+# PWM for the cooling pump
+pinStep1.freq(800)  # Frequency is the number of times per second that we repeat the on and off cycle -> rotating speed
+pinStep1.duty(400)  # Duty cycle refers the amount of time the pulse is ON -> voltage
+
+# bit-banging for the food dosing pump
+for x in range(0, 200000):
+    pinStep2.value(1)
+    utime.sleep_us(500)
+    pinStep2.value(0)
+    utime.sleep_us(500)
