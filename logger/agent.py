7da99b1cf1d072e9fccf630f1a134591f40416ea
JiananAlvin <jiananwang990@gmail.com>
2022-06-16 19:42:28 +0200

    sloved all bugs in agent.py

--- logger/agent.py
+++ logger/agent.py
@@ -10,23 +10,23 @@ class Logger:
     log_string = "%d/%d/%d %d:%d:%d -> %s"
 
     def __init__(self):
-        if self.folder in os.listdir():
+        if self.folder not in os.listdir():
             os.mkdir(self.folder)
-            self.logfile = open("./%s/%s" % (self.folder, self.filename), 'w')
-            time_params = time.localtime()
-            self.logfile.write(self.init_string % self.time_format())
+        self.logfile = open("./%s/%s" % (self.folder, self.filename), 'w')
+        self.logfile.write(self.init_string % self.time_format())
 
     def log(self, text):
-        self.logfile.write(self.log_string % self.time_format() + (text,))
+        self.logfile.write(self.log_string % (self.time_format() + (text,)))
 
     def end(self):
         self.logfile.close()
 
-    def time_format():
+    def time_format(self):
         time_params = time.localtime()
-        return (time_params.tm_mday,
-                time_params.tm_mon,
-                time_params.tm_year,
-                time_params.tm_hour,
-                time_params.tm_min,
-                time_params.tm_sec)
+        print(time_params)
+        return (time_params[2],
+                time_params[1],
+                time_params[0],
+                time_params[3],
+                time_params[4],
+                time_params[5])
