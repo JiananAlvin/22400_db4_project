fb2f5a8752c9fc8e1763da28901248f0135eed50
Paulo9608 <Paulo9608@github.com>
2022-06-17 08:21:09 +0200

    morning

--- logger/agent.py
+++ logger/agent.py
@@ -6,16 +6,22 @@ class Logger:
     folder = "logs"
     filename = "log"
     logfile = None
-    init_string = "LOG AGENT STARTED\nLocal time: %d/%d/%d %d:%d:%d"
+    init_string = "LOG AGENT STARTED\n%s\n\nLocal time: %d/%d/%d %d:%d:%d\n"
     log_string = "%d/%d/%d %d:%d:%d -> %s"
-
+    logfile_names = ["Temperature","Cooling_motor","Feeding_motor","rgbsensor"]
+    logfile_pool = []
     def __init__(self):
         if self.folder not in os.listdir():
             os.mkdir(self.folder)
-        self.logfile = open("./%s/%s" % (self.folder, self.filename), 'w')
-        self.logfile.write(self.init_string % self.time_format())
 
-    def log(self, text):
+        for name in self.logfile_names:
+            logfile = open("./%s/%s%s" % (self.folder, self.filename, name), 'w')
+            logfile.write(self.init_string % (name,self.time_format()))
+            self.logfile = logfile
+            self.logfile_pool.append(logfile)
+
+
+    def log(self, text, ):
         self.logfile.write(self.log_string % (self.time_format() + (text,)))
 
     def end(self):
